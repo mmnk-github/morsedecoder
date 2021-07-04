d718298fed80fb40a06eed76dfcec846b33e2f50
mmnk-github <48mimineko@gmail.com>
2021-07-05 01:43:00 +0900

    できた 10文字くらいなら答えが出るよ

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 # モールス
 morse_japanese = {'ア': '--.--', 'イ': '.-', 'ウ': '..-', 'エ': '-.---', 'オ': '.-...', 'カ': '.-..', 'キ': '-.-..', 'ク': '...-', 'ケ': '-.--', 'コ': '----', 'サ': '-.-.-', 'シ': '--.-.', 'ス': '---.-', 'セ': '.---.', 'ソ': '---.', 'タ': '-.', 'チ': '..-.', 'ツ': '.--.', 'テ': '.-.--', 'ト': '..-..', 'ナ': '.-.', 'ニ': '-.-.', 'ヌ': '....', 'ネ': '--.-', 'ノ': '..--', 'ハ': '-...', 'ヒ': '--..-', 'フ': '--..', 'ヘ': '.', 'ホ': '-..', 'マ': '-..-', 'ミ': '..-.-', 'ム': '-', 'メ': '-...-', 'モ': '-..-.', 'ヤ': '.--', 'ユ': '-..--', 'ヨ': '--', 'ラ': '...', 'リ': '--.', 'ル': '-.--.', 'レ': '---', 'ロ': '.-.-', 'ワ': '-.-', 'ヰ': '.-..-', 'ヱ': '.--..', 'ヲ': '.---', 'ン': '.-.-.', 'ー': '.--.-', '゛': '..', '゜': '..--.', '、': '.-.-.-'}
 
-#def 
+def replace_morse(ton, tsu):
+    new_dict = {}
+    for c, tontsu in morse_japanese.items():
+        new_tontsu = ""
+        for t in tontsu:
+            if t == '-':
+                new_tontsu = new_tontsu + tsu
+            elif t == '.':
+                new_tontsu = new_tontsu + ton
+            else:
+                print("?")
+                exit(0)
+        new_dict[c] = new_tontsu
+    return new_dict
 
 # dfs
 def dfs(crypto, answer):
+    res = []
     if crypto == "":
         print(answer)
         return
+        #nanikore = []
+        #nanikore.append(answer)
+        #return nanikore
     for c, tontsu in morse_japanese.items():
-        # print(crypto + ", " + c)
         if len(crypto) >= len(tontsu):
             if crypto[:len(tontsu)] == tontsu:
                 ansplus = answer + c
-                # print('a' + crypto[:len(tontsu)])
                 dfs(crypto[len(tontsu):], ansplus)
+                #res.extend(dfs(crypto[len(tontsu):], ansplus))
+    #return res
+    return
 
 import sys
 
@@ -28,12 +46,12 @@ if aisu == "":
     exit(0)
 
 # トンツー対応
-print("ton = ")
+print("ton(.) = ")
 ton = input()
 if ton == "":
     ton = "."
 
-print("tsu = ")
+print("tsu(-) = ")
 tsu = input()
 if tsu == "":
     tsu = "-"
@@ -42,8 +60,8 @@ if ton == tsu:
     print("何考えてんの？")
     exit(0)
 
-# 暗号を変換
-
+# 辞書を変換
+morse_japanese = replace_morse(ton, tsu)
 
 # 日本語ですか？
 print("This is Japanese? (Y/n): ")
@@ -52,11 +70,16 @@ if isJapanese == "n" or isJapanese == "N" or isJapanese == "false" or isJapanese
     # fuck
     print("fuck")
 
-
 # 貪欲に当てていく。
 # 間違えたら戻る。
+dfs(aisu, "")
+exit(0)
 
-ans = ""
-dfs(aisu, ans)
-
+ans = dfs(aisu, "")
+print("count: " + str(len(ans)))
+if len(ans) == 0:
+    print("残念")
+    exit(0)
+ans.sort()
+print(ans)
 print("finish.")
